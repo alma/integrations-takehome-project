c53045ab396264404e6bc3f922a632453b326e3d
Olivier Lance <olivier.lance@getalma.eu>
2020-09-02 13:57:15 +0200

    Improves API serving script:
    - Auto-create specific network if needed
    - Automatically removes existing `alma` container before creating a new one

--- serve.sh
+++ serve.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 
+# Create `exo-alma` network if needed
+if [[ $(docker network ls --quiet --filter name=exo-alma | wc -c) -eq 0 ]]; then
+  docker network create exo-alma
+fi
+
+# Remove older `alma` container if there's one
+if [[ $(docker container ls --quiet --all --filter name=alma | wc -c) -ne 0 ]]; then
+  docker rm alma &> /dev/null
+fi
+
 PORT=${PORT:-5000}
-docker build -t alma:fake . && docker run -it -e PORT=$PORT -p $PORT:$PORT -v "$PWD/src":/usr/src/app alma:fake
+docker build -t alma:fake . && docker run -it --network exo-alma -e PORT=$PORT -p $PORT:$PORT --name=alma -v "$PWD/src":/usr/src/app alma:fake
